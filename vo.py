a6f1addafa007a88b75e3fee49c40baef8750510
vusanthiya <vusanthiya@gmail.com>
2019-06-12 14:19:05 +0530

    Create vo.py

---
+++ vo.py
@@ -0,0 +1,9 @@
+a=input()
+b=0
+for i in a:
+  if(i=="a" or i=="e" or i=="i" or i=="o" or i=="u" or i=="A" or i=="E" or i=="I" or i=="O" or i=="U"):
+    b=1
+if(b==1):
+  print("yes")
+else:
+  print("No") 
